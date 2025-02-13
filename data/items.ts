b9acce52951aa80ec6aab1f81536c69c28b867e2
Zerapium <mayurhiwale2451982@gmail.com>
2025-02-13 14:54:06 +0530

    [Automated Push] undefined

--- data/items.ts
+++ data/items.ts
@@ -8489,6 +8489,9 @@ export const Items: import("../sim/dex-items").ItemDataTable = {
 
 
 
+
+
+
 /*CUSTOM ITEMS*/
  "lucarionitex":{"name":"Lucarionite X","itemUser":["Lucario"],"megaEvolves":"Lucario","num":-1002,"isNonstandard":"Unobtainable","gen":0}
 };
