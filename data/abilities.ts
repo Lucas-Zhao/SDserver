c933367cd1b85f8e0750f7b458bf19b55ee90202
Zerapium <mayurhiwale2451982@gmail.com>
2025-02-17 13:45:55 +0530

    [Automated Push] Data Update

--- data/abilities.ts
+++ data/abilities.ts
@@ -6176,7 +6176,7 @@ Abilities["antomize"].onModifyType = function(move, pokemon) {
 			) {
 				move.type = "Bug";
 				move.typeChangerBoosted = this.effect;
-			}
+			
 		
 
 Abilities["antomize"].onBasePower = function(basePower, pokemon, target, move) {
--- data/items.ts
+++ data/items.ts
@@ -8662,6 +8662,7 @@ export const Items: import("../sim/dex-items").ItemDataTable = {
 
 
 
+
 /*CUSTOM ITEMS*/
  "lucarionitex":{"name":"Lucarionite X","itemUser":["Lucario"],"megaEvolves":"Lucario","megaStone":"Lucario-Mega-X","num":-1002,"isNonstandard":"Unobtainable","gen":0}
 };
