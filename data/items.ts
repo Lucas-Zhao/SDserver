3889be7e06f59ac03e89530366c6f13cfe76f41f
Lucas-Zhao <lucas.zhao2002@gmail.com>
2025-02-21 15:21:45 +0000

    [Automated Push] Data Update

--- data/items.ts
+++ data/items.ts
@@ -8400,10 +8400,16 @@ export const Items: import("../sim/dex-items").ItemDataTable = {
 
 
 
+
+
+
+
+
 /*CUSTOM ITEMS*/
  "lucarionitex":{"name":"Lucarionite X","megaStone":"Lucario-Mega-X","itemUser":["Lucario"],"megaEvolves":"Lucario","num":-1003,"gen":0,"isNonstandard":"Unobtainable"}
 , "serperionite":{"name":"Serperionite","megaStone":"Serperior-Mega","itemUser":["Serperior"],"megaEvolves":"Serperior","num":-1003,"isNonstandard":"Unobtainable","gen":0}
 , "serperiorite":{"name":"Serperiorite","megaStone":"Serperior-Mega","itemUser":["Serperior"],"megaEvolves":"Serperior","num":-1003,"isNonstandard":"Unobtainable","gen":0}
+, "emboarite":{"name":"Emboarite","megaStone":"Emboar-Mega","itemUser":["Emboar"],"megaEvolves":"Emboar","num":-1004,"isNonstandard":"Unobtainable","gen":0}
 
 };
 /*FUNCTIONS*/
@@ -8428,4 +8434,12 @@ Items["serperiorite"].onTakeItem = function(item, source) {
         return true;
       }
 
-//serperioriteend
+//serperioriteend//emboaritestart
+
+Items["emboarite"].onTakeItem = function(item, source) {
+        if (item.megaEvolves === source.baseSpecies.baseSpecies)
+          return false;
+        return true;
+      }
+
+//emboariteend
--- data/learnsets.ts
+++ data/learnsets.ts
@@ -99717,4 +99717,5 @@ export const Learnsets: import('../sim/dex-species').LearnsetDataTable = {
 /*CUSTOM LEARNSETS*/
  "lucariomegax":{"learnset":{"aurasphere":["9L4","9L3"]}}
 , "serperiormega":{"learnset":{"energyball":["9L4","9L3"],"gigadrain":["9L4","9L3"]}}
+, "emboarmega":{"learnset":{"bodyslam":["9L4","9L3"],"firepunch":["9L4","9L3"]}}
 };
