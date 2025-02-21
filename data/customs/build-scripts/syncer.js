2b9fb813db01d4d825286d8da8e8555b7cfb8125
Lucas-Zhao <lucas.zhao2002@gmail.com>
2025-02-21 11:47:33 -0500

    change

--- data/customs/build-scripts/syncer.js
+++ data/customs/build-scripts/syncer.js
@@ -1,14 +1,14 @@
 /*
  * syncer.js
  * Author - Pokem9n | https://github.com/ISenseAura
- * 
+ *
  * This file contains logic to sync data from main PS' repository,
- * also updates local and remote server repository then attempts to 
- * sync client and server data. 
+ * also updates local and remote server repository then attempts to
+ * sync client and server data.
  * This is primarily used after updating custom data in server
- * through API. The new custom data needs to be updated in 
+ * through API. The new custom data needs to be updated in
  * client repository as well thats what this script does.
- * 
+ *
  */
 
 const fs = require("fs");
@@ -22,7 +22,7 @@ let updateRepo = (msg = "Data Update") => {
 	console.log("\n[UPDATING SERVER REPO]")
 	console.log(execSync("git add .",{cwd : MAIN_PATH + "/" + SERVER_FOLDER_NAME}).toString())
 	console.log(execSync(`git commit -m "[Automated Push] ${msg}"`,{cwd : MAIN_PATH + "/" + SERVER_FOLDER_NAME}).toString())
-	console.log(execSync(`git push origin2`,{cwd : MAIN_PATH + "/" + SERVER_FOLDER_NAME}).toString())
+	console.log(execSync(`git push origin`,{cwd : MAIN_PATH + "/" + SERVER_FOLDER_NAME}).toString())
 	console.log("\n[FINISHED]")
 }
 
