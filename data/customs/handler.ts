b7fab3bc4b0491b35910f9d1d581f7be98e74b9e
Zerapium <mayurhiwale2451982@gmail.com>
2025-02-22 18:45:54 +0530

    [Automated Push] Data Update

--- data/customs/handler.ts
+++ data/customs/handler.ts
@@ -569,6 +569,10 @@ export class Handler {
 	deletePokemon(name: string) {
 		let id = this.toID(name);
 		delete this.pokedex[id];
+		if(this.pokedex[id].requiredItem) {
+			delete this.items[this.toID(this.pokedex[id].requiredItem)]
+			this.convertToTxt("items");
+		}
 		delete this.formatsdata[id];
 		delete this.learnsets[id];
 		delete this.texts.pokedex[id];
@@ -576,6 +580,7 @@ export class Handler {
 		this.convertToTxt("learnsets");
 		this.convertToTxt("formats-data");
 		this.convertToTxt("pokedex", true);
+		this.updateFormat();
 		this.importAll();
 	}
 
