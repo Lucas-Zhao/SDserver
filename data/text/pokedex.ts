0e1616f7d13a60db36c2dfda6a11024deb4ca894
Lucas-Zhao <lucas.zhao2002@gmail.com>
2025-02-26 18:53:42 +0000

    [Automated Push] Data Update

--- data/text/pokedex.ts
+++ data/text/pokedex.ts
@@ -3783,6 +3783,7 @@ export const PokedexText: {[id: IDEntry]: PokedexText} = {
 , "ironclaw":{"name":"Ironclaw"}
 , "dragapultmega":{"name":"Dragapult-Mega"}
 , "ironpod":{"name":"Ironpod"}
+, "shatterjaw":{"name":"Shatterjaw"}
 
 };
  
