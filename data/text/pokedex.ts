55431ce30bed0f93f49db9230b85b75995c2fae7
Lucas-Zhao <lucas.zhao2002@gmail.com>
2025-02-26 01:32:53 +0000

    [Automated Push] Data Update

--- data/text/pokedex.ts
+++ data/text/pokedex.ts
@@ -3777,6 +3777,7 @@ export const PokedexText: {[id: IDEntry]: PokedexText} = {
 , "dusknoirmega":{"name":"Dusknoir-Mega"}
 , "comfeymega":{"name":"Comfey-Mega"}
 , "corviknightmega":{"name":"Corviknight-Mega"}
+, "clashingteeth":{"name":"Clashingteeth"}
 
 };
  
