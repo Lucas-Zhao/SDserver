1aab0c694339b7538a9e45b9f7bf38009a27b38f
Lucas-Zhao <lucas.zhao2002@gmail.com>
2025-05-26 17:18:08 +0000

    [Automated Push] Data Update

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -6285,5 +6285,6 @@ export const FormatsData: import('../sim/dex-species').SpeciesFormatsDataTable =
 , "munkidoriunchained":{"isNonstandard":"Unobtainable","tier":"HELL","natDexTier":"HELL"}
 , "okidogiunchained":{"isNonstandard":"Unobtainable","tier":"HELL","natDexTier":"HELL"}
 , "raltstrenchmaiden":{"isNonstandard":"Unobtainable","tier":"HELL","natDexTier":"HELL"}
+, "kirliatrenchmaiden":{"isNonstandard":"Unobtainable","tier":"HELL","natDexTier":"HELL"}
 
 };
